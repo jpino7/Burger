01c082e0ea9689c63bd8ac7327174027c9709c4d
JRPino <pino3789@gmail.com>
2020-05-04 17:01:35 -0700

    Created burgers_db, burgers table and Inserts

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+  
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Famous Star with Cheese', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Bacon Western Cheeseburger', FALSE);
+INSERT INTO BURGERS (burger_name, devoured) VALUES ('Low Carb Famous Star', FALSE);
