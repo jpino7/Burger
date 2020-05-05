2196a3b032be5b1cf24120229cbe663d9b80861d
JRPino <pino3789@gmail.com>
2020-05-05 14:57:51 -0700

    Created code for burger.js to go with orm code

--- models/burger.js
+++ models/burger.js
@@ -5,7 +5,39 @@ function burger(name) {
     this.devoured = false;
 }
 
+burger.selectBurgers = function () {
+    return new Promise((resolve, reject) => {
+        orm.selectAll("BURGERS").then(results => {
+            resolve(results);
+        }).catch(() => {
+            reject("Could not get burgers");
+        });
+    });
+};
 
+burger.create = function (burger) {
+    return new Promise((resolve, reject) => {
+        orm.insertOne("BURGERS", {
+            burger_name: burger.name,
+            devoured: burger.devoured
+        }).then(results => {
+            // DB generated burgerID
+            burger.id = results.insertId;
+            resolve(burger.id);
+        }).catch(() => {
+            reject("Could not add burger");
+        });
+    });
+};
 
+burger.updateDevoured = function (burgerId) {
+    return new Promise((resolve, reject) => {
+        orm.updateOne("BURGERS", "DEVOURED", true, "ID", burgerId).then(results => {
+            resolve(results);
+        }).catch(() => {
+            reject("Could not update burger");
+        });
+    });
+};
 
 module.exports = burger;
