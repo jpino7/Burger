999dea82d4cf223a6efd57cefbda38c817536422
JRPino <pino3789@gmail.com>
2020-05-07 22:14:21 -0700

    Added JawsDB PW

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,7 @@ if (process.env.JAWSDB_URL) {
         database: "burger_db",
         user: "root",
         port: 3306,
-        password: "rootroot"
+        password: "gys1ic3ohsxv8iwk"
     });
 }
 
